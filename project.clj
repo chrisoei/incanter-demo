5efbfb3f7fd319eed75fce6ea2ac5c57e6a15e09
Chris Oei <c@c-oei.com>
2016-09-04 09:41:44 -0700

    Add incanter and stardate deps and force tests to pass

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.c-oei/stardate "5.0.0-SNAPSHOT"]
+                 [incanter "1.9.1"]])
