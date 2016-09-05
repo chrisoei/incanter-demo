d0ad020078ffd97151fdfa93467ff178fb4f1399
Chris Oei <c@c-oei.com>
2016-09-04 22:14:32 -0700

    Use shorthand in namespaces

--- src/incanter_demo/core.clj
+++ src/incanter_demo/core.clj
@@ -1,4 +1,6 @@
 (ns incanter-demo.core
-   (:require [incanter [core stats charts]]
-             [stardate [core]]))
+   (:require [incanter [core :refer :all]
+                       [stats :refer :all]
+                       [charts :refer :all]]
+             [stardate [core :as sd]]))
 
