3e4401d30db61c98bbfb869c43cdd8aab6fc0d6e
Chris Oei <c@c-oei.com>
2016-09-04 22:18:19 -0700

    Add incanter.excel to the ns

--- src/incanter_demo/core.clj
+++ src/incanter_demo/core.clj
@@ -1,6 +1,7 @@
 (ns incanter-demo.core
    (:require [incanter [core :refer :all]
-                       [stats :refer :all]
-                       [charts :refer :all]]
+                       [charts :refer :all]
+                       [excel :refer :all]
+                       [stats :refer :all]]
              [stardate [core :as sd]]))
 
