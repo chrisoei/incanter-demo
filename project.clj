626c4875ef36da2d0c000334a9b1eeb20a59cfcf
Chris Oei <c@c-oei.com>
2016-09-04 21:18:57 -0700

    Set initial ns and add GitHub URL

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject incanter-demo "0.1.0-SNAPSHOT"
   :description "Demo project for time series analysis with Incanter"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/chrisoei/incanter-demo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.c-oei/stardate "5.0.0-SNAPSHOT"]
-                 [incanter "1.9.1"]])
+                 [incanter "1.9.1"]]
+  :repl-options {:init-ns incanter-demo.core})
